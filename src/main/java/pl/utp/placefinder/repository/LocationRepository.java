44b28b9d52041b3c6a0cca934c42554173c1e746
hubigabi <hubigabi19@gmail.com>
2020-05-04 16:33:34 +0200

    Changed current position of users - for testing

--- src/main/java/pl/utp/placefinder/repository/LocationRepository.java
+++ src/main/java/pl/utp/placefinder/repository/LocationRepository.java
@@ -3,8 +3,13 @@ package pl.utp.placefinder.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.utp.placefinder.model.Location;
+import pl.utp.placefinder.model.LocationType;
+
+import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    List<Location> findAllByLocationType(LocationType locationType);
 
 }
--- src/main/java/pl/utp/placefinder/service/InitService.java
+++ src/main/java/pl/utp/placefinder/service/InitService.java
@@ -2,23 +2,34 @@ package pl.utp.placefinder.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.utp.placefinder.model.Location;
+import pl.utp.placefinder.model.LocationType;
 import pl.utp.placefinder.model.Place;
 import pl.utp.placefinder.model.User;
+import pl.utp.placefinder.repository.LocationRepository;
 import pl.utp.placefinder.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import rx.*;
 
 @Service
 public class InitService {
 
     private PlaceService placeService;
     private UserRepository userRepository;
+    private LocationRepository locationRepository;
 
     @Autowired
-    public InitService(PlaceService placeService, UserRepository userRepository) {
+    public InitService(PlaceService placeService, UserRepository userRepository, LocationRepository locationRepository) {
         this.placeService = placeService;
         this.userRepository = userRepository;
+        this.locationRepository = locationRepository;
     }
 
     @PostConstruct
@@ -34,5 +45,40 @@ public class InitService {
         u2 = userRepository.save(u2);
         u3 = userRepository.save(u3);
 
+
+        Location l1 = new Location(0L, 53.150849, 18.132337, LocationType.HOME, u1);
+        Location l2 = new Location(0L, 53.150127, 18.143287, LocationType.WORK, u1);
+        Location l3 = new Location(0L, 53.150712, 18.13982, LocationType.CURRENT_LOCATION, u1);
+
+        Location l4 = new Location(0L, 52.150949, 18.132337, LocationType.HOME, u2);
+        Location l5 = new Location(0L, 53.150127, 19.843287, LocationType.WORK, u2);
+        Location l6 = new Location(0L, 52.180712, 17.13982, LocationType.CURRENT_LOCATION, u2);
+
+        Location l7 = new Location(0L, 53.150849, 16.132337, LocationType.HOME, u3);
+        Location l8 = new Location(0L, 53.150127, 19.143287, LocationType.WORK, u3);
+        Location l9 = new Location(0L, 54.150712, 18.13982, LocationType.CURRENT_LOCATION, u3);
+
+        locationRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5, l6, l7, l8, l9));
+
+
+        //For testing
+        //Current position of all users is changing randomly every 5 seconds
+        Observable.interval(5, TimeUnit.SECONDS)
+                .subscribe(aLong -> {
+
+                    Random random = new Random();
+                    locationRepository.saveAll(
+                            locationRepository.findAllByLocationType(LocationType.CURRENT_LOCATION)
+                                    .stream()
+                                    .map(location -> {
+                                        location.setLat(location.getLat() + random.nextDouble() / 10000);
+                                        location.setLng(location.getLng() + random.nextDouble() / 10000);
+                                        System.out.println(location);
+                                        return location;
+                                    })
+                                    .collect(Collectors.toList())
+                    );
+
+                });
     }
 }
